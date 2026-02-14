d4b9b66bf2758c69fc3e30c8de4d1c9124166334
Dhruvdesai793 <blixture793@gmail.com>
2026-02-14 19:09:03 +0530

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { PageTransitionProvider } from './components/PageTransition'
 
